be7004528beb7c779d5393e4fc8a45d6511e3eb3
Jack Royal <jack.royal@intelia.com.au>
2023-05-26 12:33:04 +1000

    Test push 8

--- modules/gcs/main.tf
+++ modules/gcs/main.tf
@@ -1,5 +1,5 @@
-resource "google_storage_bucket" "test-bucket" {
-  name     = "test-bucket"
+resource "google_storage_bucket" "test-bucket-jackintelia" {
+  name     = "test-bucket-jackintelia"
   location = var.region
   project  = var.project
 
